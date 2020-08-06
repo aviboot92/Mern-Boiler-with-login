bde26d8df61654e2efb81a51d0b68dd51f059f03
avkarra <avkarra@ncompasbusiness.com>
2020-08-06 18:47:02 +0530

    .env is added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
@@ -17,6 +18,6 @@ app.use('/api/profile' , require('./routes/api/profile'));
 app.use('/api/posts' , require('./routes/api/posts'));
 app.use('/api/auth' , require('./routes/api/auth'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server has started at ${PORT}`));
