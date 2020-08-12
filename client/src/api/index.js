78474b844d3da48afeb851740932f3371a8ecacb
avkarra <avkarra@ncompasbusiness.com>
2020-08-12 14:25:06 +0530

    removed interceptor

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import {LOGOUT} from 'variables/auth';
-import {store} from 'redux/store';
-
 
 
 // Set config defaults when creating the instance
@@ -12,21 +9,6 @@ const baseAxios = axios.create({
     }
 });
 
-/**
- intercept any error responses from the api
- and check if the token is no longer valid.
- ie. Token has expired
- logout the user if the token has expired
-**/
 
-baseAxios.interceptors.response.use(
-    res => res,
-    err => {
-      if (err.response.data.msg === 'Token is not valid') {
-        store.dispatch({ type: LOGOUT });
-      }
-      return Promise.reject(err);
-    }
-  );
 
 export default baseAxios;
